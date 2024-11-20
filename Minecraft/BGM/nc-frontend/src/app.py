25dc5817873fda6a5628de14e1bc07557eb45a6c
Niilyx _ <43385037+Niilyx@users.noreply.github.com>
2024-11-21 00:42:47 +0100

    ...darnit

--- Minecraft/BGM/nc-frontend/src/app.py
+++ Minecraft/BGM/nc-frontend/src/app.py
@@ -52,23 +52,18 @@ def main():
     except ConnectionRefusedError as e:
         exit(e.errno)
 
-    with (s.socket(s.AF_INET, s.SOCK_STREAM) as this_server):
-        this_server.bind(("127.0.0.1", LISTEN_PORT))
-        this_server.listen(1)
-        client, client_info = this_server.accept()
-
-        java_socket, ping = forward_message(
-            java_socket,
-            ("check_queue\n" + os.environ.get('CONTAINER_CREATOR') + "\n").encode()
-        )
-        ping = ping.strip(b" \r\n")
-        if ping == b"dead":
-            print(b"Une erreur est survenue...")
-            return
-        elif ping == b"in_queue":
-            print(b"Oi ! Tu crois que t'es seul ici ?! Attends ton tour !" + b"\n")
-            return
-        print(b"""Bon, je vais pas tourner autour du pot.
+    java_socket, ping = forward_message(
+        java_socket,
+        ("check_queue\n" + os.environ.get('CONTAINER_CREATOR') + "\n").encode()
+    )
+    ping = ping.strip(b" \r\n")
+    if ping == b"dead":
+        print(b"Une erreur est survenue...")
+        return
+    elif ping == b"in_queue":
+        print(b"Oi ! Tu crois que t'es seul ici ?! Attends ton tour !" + b"\n")
+        return
+    print(b"""Bon, je vais pas tourner autour du pot.
 Entre ton mot a 48 bits et je te dirai si lumiere sera... ou pas.
 Format : e0e1e2...e45e46e47
 Exemple : 101010101010101010101010101010101010101010101010
@@ -77,36 +72,36 @@ Bonne chance :)
 
 
 >>> """)
-        answer = client.recv(64).decode().strip()
-        if len(answer) != 48:
-            print("Pfff, serieusement ? J'ai dit 48 bits ! Allez, j'me tire.".encode())
-            return
-        elif any(c not in "01" for c in answer):
-            print("T'as pas compris ce que je t'ai dit ??? C'est des 0 et des 1 et rien d'autre !".encode())
-            return
+    answer = client.recv(64).decode().strip()
+    if len(answer) != 48:
+        print("Pfff, serieusement ? J'ai dit 48 bits ! Allez, j'me tire.".encode())
+        return
+    elif any(c not in "01" for c in answer):
+        print("T'as pas compris ce que je t'ai dit ??? C'est des 0 et des 1 et rien d'autre !".encode())
+        return
 
-        print("En voila une belle reponse ! Je reviens vers toi, un instant...\n\n".encode())
+    print("En voila une belle reponse ! Je reviens vers toi, un instant...\n\n".encode())
 
-        java_socket, java_res = forward_message(
-            java_socket,
-            ("solution\n" + os.environ.get('CONTAINER_CREATOR') + "\n" + answer + "\n").encode()
-        )
+    java_socket, java_res = forward_message(
+        java_socket,
+        ("solution\n" + os.environ.get('CONTAINER_CREATOR') + "\n" + answer + "\n").encode()
+    )
 
-        java_res = java_res.strip(b" \r\n").decode()
-        if java_res == "dead":
-            print("Une erreur est survenue...".encode())
-        elif java_res == "will_do":
-            response = java_socket.recv(1024).decode().strip(" \r\n")
+    java_res = java_res.strip(b" \r\n").decode()
+    if java_res == "dead":
+        print("Une erreur est survenue...".encode())
+    elif java_res == "will_do":
+        response = java_socket.recv(1024).decode().strip(" \r\n")
 
-            if response == "win":
-                print("Wow, j'y crois pas !\n".encode())
-                print("Voici ton flag : ".encode() + FLAG)
-            elif response == "lose":
-                print("Dommage, ce n'est pas le bon mot...".encode())
-            elif response == "in_queue":
-                print("Hé ! T'as cru que j'allais te donner la réponse comme ça ?! Attends un peu !".encode())
-        else:
-            print("Une erreur est survenue...".encode())
+        if response == "win":
+            print("Wow, j'y crois pas !\n".encode())
+            print("Voici ton flag : ".encode() + FLAG)
+        elif response == "lose":
+            print("Dommage, ce n'est pas le bon mot...".encode())
+        elif response == "in_queue":
+            print("Hé ! T'as cru que j'allais te donner la réponse comme ça ?! Attends un peu !".encode())
+    else:
+        print("Une erreur est survenue...".encode())
 
 
 if __name__ == "__main__":
